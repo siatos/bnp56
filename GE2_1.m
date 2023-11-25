dcd32802991036d7d578b521674a7704cdbcae74
s3e3 <48084258+siatos@users.noreply.github.com>
2023-11-25 13:34:32 +0200

    Add files via upload

--- GE2_1.m
+++ GE2_1.m
@@ -1,24 +1,23 @@
-%%angle_vec_ = [10*pi/180 20*pi/180 30*pi/180]
-
-angle_vec = [10 20 30]
-angle_vec = deg2rad(angle_vec)  
+%%
+angle_vec = [10 20 30]          %% in degreees      
+angle_vec = deg2rad(angle_vec)  %% in rads
 
 %% initial points
-A1 = [0 0 0]
-B1 = [1 1 1]
-C1 = [2 2 2]
+A1 = [0; 0; 0]
+B1 = [1; 1; 1]
+C1 = [2; 2; 2]
 
-final_rotation = rotation_matrix(angle_vec)
+R = rotation_matrix(angle_vec)  %% rotation matrix
 
-A2 = A1 * final_rotation
-B2 = B1 * final_rotation
-C2 = C1 * final_rotation
+A2 = R * A1
+B2 = R * B1
+C2 = R * C1
 
 DA = euclid_dist(A1, A2)
 DB = euclid_dist(B1, B2)
 DC = euclid_dist(C1, C2)
 
-
+%% Function Definitions
 
 %% Function definition for deg2rad  %%%
 %% Converts a 1x3 vector expressed in degrees
@@ -33,10 +32,22 @@ function rad_vec = deg2rad(deg_vec)
    rad_vec = (deg_vec * pi)/180
 end
 
+%% since we will apply matrix multiplication 
+%% each New_Point is derived from the Initial_Point using the formula
+%% New_Point = (Rz*Ry*Rx)*Initial_Point where
+%% Rx is the Pitch rotation around x-axis
+%% Ry is the Yaw rotation around y-axis
+%% Rz is the Roll rotation around z-axis
+%% see:
+%% Glabe, J. (2021). Euler Angles and Roll Pitch Yaw Representation. 
+%% In: Ang, M.H., Khatib, O., Siciliano, B. (eds) Encyclopedia of Robotics. 
+%% Springer, Berlin, Heidelberg. https://doi.org/10.1007/978-3-642-41610-1_129-1 
+
+
 %% Function definition for euler transformation %%%
 %% Uses the euler tranformation matrices (3x3) to
 %% calculate rotation matrix
-%% angle_vector: 1x3 vector (values in rads)
+%% angle_vector: 3x1 vector column (values in rads)
 %% return: rotation matrix (3x3)
 function rot_matrix = rotation_matrix(angle_vec)
    psi = angle_vec(1,1)
@@ -46,17 +57,17 @@ function rot_matrix = rotation_matrix(angle_vec)
    Ry = [cos(th) 0 sin(th); 0 1 0; -sin(th) 0 cos(th)]
    Rz = [cos(phi) -sin(phi) 0; sin(phi) cos(phi) 0; 0 0 1]
 
-   rot_matrix = Rx*Ry*Rz
+   rot_matrix = Rz*Ry*Rx
 end
 
 
 %% Function definition for euclidean distance %%%
 %% Takes as input 2 3D points - 2 vectors 1x3
 %% and returns the euclidean distance
-%% start_point: 1x3 vector
-%% end_point:   1x3 vector
+%% start_point: 3x1 vector column
+%% end_point:   3x1 vector column
 %% return:      number
 function res = euclid_dist(start_point, end_point)
   diff_vect = end_point - start_point 
-  res = sqrt(diff_vect(1,1)^2 + diff_vect(1,2)^2 +diff_vect(1,3)^2)
+  res = sqrt(diff_vect(1,1)^2 + diff_vect(2,1)^2 +diff_vect(3,1)^2)
 end
