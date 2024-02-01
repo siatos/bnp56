2cd04ba1e9b5183b1d48eda845f48788e874f7e8
s3e3 <48084258+siatos@users.noreply.github.com>
2024-02-01 15:32:45 +0200

    Update update_GE3_1_short.m

--- update_GE3_1_short.m
+++ update_GE3_1_short.m
@@ -5,10 +5,18 @@ fused_img = (mri_img + pet_img)/2
 normalized_img = normalize_image(fused_img)
 
 
-subplot(1, 4, 1), imshow(mri_img), title('MRI image')
-subplot(1, 4, 2), imshow(pet_img), title('PET image')
-subplot(1, 4, 3), imshow(fused_img), title('FUSED image')
-subplot(1, 4, 4), imshow(normalized_img), title('Normalized image')
+subplot(1, 4, 1)
+imshow(mri_img)
+title('MRI image')
+subplot(1, 4, 2)
+imshow(pet_img)
+title('PET image')
+subplot(1, 4, 3)
+imshow(fused_img)
+title('FUSED image')
+subplot(1, 4, 4)
+imshow(normalized_img)
+title('Normalized image')
 
 %% Function normalize image %%
 function out = normalize_image(input_img)
